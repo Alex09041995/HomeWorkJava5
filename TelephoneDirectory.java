b0f93e011578127788fc877a31870e28ffc76066
«Alexander» <shmygarevalexander009@gmail.com>
2023-09-23 11:27:47 +0500

    TelephoneDirectory.java

---
+++ TelephoneDirectory.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class TelephoneDirectory {
+
+    public void add(String lastName, String phoneNumber) {
+    }
+
+    public List<String> get(String lastName) {
+        return null;
+    }
+
+    public void remove(String lastName) {
+    }
+
+    public void list() {
+    }
+
+    public void addContact(String string, String string2) {
+    }
+
+    public static List<String> getPhoneNumbers(String string) {
+        return null;
+    }
+
+}
+
+
+
+
+
